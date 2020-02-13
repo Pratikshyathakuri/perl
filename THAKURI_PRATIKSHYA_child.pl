e31d911b6fb22b9295558e533c6b5008259fb266
Pratikshyathakuri <thakuripratikshya1@gmail.com>
2020-02-12 22:47:44 -0600

    sff

--- THAKURI_PRATIKSHYA_child.pl
+++ THAKURI_PRATIKSHYA_child.pl
@@ -20,10 +20,9 @@ or die "Error Opening the File";
 my $numLine = 0;
 while (my $line = <$fh>){
     $numLine++;
-    chomp $line
      my $toWrite;
     if($line){
-         $toWrite = 'Child['. $pid .']: Line['. $numLine .']: '.  $line . "\n";
+         $toWrite = 'Child['. $pid .']: Line['. $numLine .']: '.  $line ;
     }
   
     print $fh2 $toWrite;
