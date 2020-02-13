d9a401c83f957b48fd40bbbc14bd8b0e6ef47c91
Pratikshyathakuri <thakuripratikshya1@gmail.com>
2020-02-12 22:40:07 -0600

    sff

--- THAKURI_PRATIKSHYA_child.pl
+++ THAKURI_PRATIKSHYA_child.pl
@@ -22,8 +22,10 @@ my $numLine = 0;
 while (my $line = <$fh>){
     $numLine++;
     chomp $line;
-    $line;
-    my $toWrite =  'Child['. $pid .']: Line['. $numLine .']: '.  $line + "\n";
+    if($line){
+        my $toWrite =  'Child['. $pid .']: Line['. $numLine .']: '.  $line + "\n";
+    }
+  
     print $fh2 $toWrite;
 }
 
